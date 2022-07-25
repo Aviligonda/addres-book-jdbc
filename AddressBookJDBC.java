41154d902b7feeb78eb391f286b36ec54bb1da71
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-25 13:20:06 +0530

    ADDED : Creating a new contact from JDBC

--- AddressBookJDBC.java
+++ AddressBookJDBC.java
@@ -12,9 +12,20 @@ public class AddressBookJDBC {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/address_book_service", "root", "Sravani@6086");
             System.out.println("Connection Done...");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM address_book WHERE state='Andhra' ");
+            statement.execute("INSERT INTO address_book VALUES('Danial','Raj','BJP','ATP','ATP','Danial@gamil.com','515701','1233456','AP','Business','8','2022-05-26')");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM address_book  ");
             while (resultSet.next()) {
-                System.out.println("Andhra state matches with contacts is : " + resultSet.getInt(1));
+                System.out.println(resultSet.getString(1)
+                        + " " + resultSet.getString(2)
+                        + " " + resultSet.getString(3)
+                        + " " + resultSet.getString(4)
+                        + " " + resultSet.getString(5)
+                        + " " + resultSet.getString(6)
+                        + " " + resultSet.getInt(7)
+                        + " " + resultSet.getInt(8)
+                        + " " + resultSet.getString(9)
+                        + " " + resultSet.getString(10)
+                        + " " + resultSet.getInt(11));
             }
             connection.close();
         } catch (Exception e) {
