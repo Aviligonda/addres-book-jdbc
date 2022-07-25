0d84dbaa996164b4d633666a930a2c715ad5ea6d
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-25 12:17:05 +0530

    Welcome message

---
+++ AddressBookJDBC.java
@@ -0,0 +1,7 @@
+package com.bridzelab;
+
+public class AddressBookJDBC {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program on JDBC ...");
+    }
+}
